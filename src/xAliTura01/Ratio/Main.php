0b2dbc80b62fa015072201ee77c5e3911a51fd95
xAliTura01 <cetinalitugra@gmail.com>
2021-10-27 19:21:47 +0300

    v0.2 test 2

--- src/xAliTura01/Ratio/Main.php
+++ src/xAliTura01/Ratio/Main.php
@@ -6,6 +6,7 @@ use pocketmine\Player;
 use pocketmine\item\Item;
 use pocketmine\block\Block;
 use pocketmine\math\Vector3;
+use pocketmine\utils\Config;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\block\BlockBreakEvent;
@@ -16,6 +17,20 @@ use pocketmine\network\mcpe\protocol\PlaySoundPacket;
 
 class Main extends PluginBase implements Listener
 {
+     private $LangConfig = [];
+     private static $instance;
+
+     public function onLoad() : void 
+     {
+    	@mkdir($this->getDataFolder());
+        $this->saveDefaultConfig();
+        $this->saveResource('en_EN.yml');
+        $this->saveResource('tr_TR.yml');
+        $this->saveResource('config.yml');
+        $this->LangConfig = ["en_EN" => new Config($this->getDataFolder() . "en_EN.yml", Config::YAML),
+                             "tr_TR" => new Config($this->getDataFolder() . "tr_TR.yml", Config::YAML)
+                            ];
+	}
 
 	public function onEnable() : void
 	{
@@ -89,46 +104,56 @@ class Main extends PluginBase implements Listener
 			case 1:
 				$world->setBlock(new Vector3($x,$y,$z), Block::get(16));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["vip-message"]);
 			break;
 			case 2:
 				$world->dropItem(new Vector3($x,$y,$z), Item::get(266));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["vip-message"]);
 			break;
 			case 3:
 				$world->setBlock(new Vector3($x,$y,$z), Block::get(14));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["vip-message"]);
 			break;
 			case 4:
 				$world->dropItem(new Vector3($x,$y,$z), Item::get(370));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["vip-message"]);
 			break;
 		}
 		switch ($ratio2) {
 			case 1:
 				$world->setBlock(new Vector3($x,$y,$z), Block::get(15));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["vip-message"]);
 			break;
 			case 2:
 				$world->dropItem(new Vector3($x,$y,$z), Item::get(266));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["vip-message"]);
 			break;
 			case 3:
 				$world->setBlock(new Vector3($x,$y,$z), Block::get(129));
-				$this->playMusic($player);
+				$this->playMusic($player); 
+				$player->sendMessage($this->plugin->messages["vip-message"]);
 			break;
 			case 4:
 				$world->dropItem(new Vector3($x,$y,$z), Item::get(265));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["vip-message"]);
 			break;
 		}
 		switch($ratio3){
 			case 1:
 				$world->setBlock(new Vector3($x,$y,$z), Block::get(56));
-				$this->playMusic($player);
+				$this->playMusic($player); 
+				$player->sendMessage($this->plugin->messages["vip-message"]);
 			break;
 			case 2:
 				$world->dropItem(new Vector3($x,$y,$z), Item::get(264));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["vip-message"]);
 			break;
 		}
 	}
@@ -148,46 +173,56 @@ class Main extends PluginBase implements Listener
 			case 1:
 				$world->setBlock(new Vector3($x,$y,$z), Block::get(370));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["player-message"]);
 			break;
 			case 2:
 				$world->dropItem(new Vector3($x,$y,$z), Item::get(265));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["player-message"]);
 			break;
 			case 3:
 				$world->setBlock(new Vector3($x,$y,$z), Block::get(266));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["player-message"]);
 			break;
 			case 4:
 				$world->dropItem(new Vector3($x,$y,$z), Item::get(264));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["player-message"]);
 			break;
 		}
 		switch ($ratio2) {
 			case 1:
 				$world->setBlock(new Vector3($x,$y,$z), Block::get(370));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["player-message"]);
 			break;
 			case 2:
 				$world->dropItem(new Vector3($x,$y,$z), Item::get(265));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["player-message"]);
 			break;
 			case 3:
 				$world->setBlock(new Vector3($x,$y,$z), Block::get(266));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["player-message"]);
 			break;
 			case 4:
 				$world->dropItem(new Vector3($x,$y,$z), Item::get(266));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["player-message"]);
 			break;
 		}
 		switch ($ratio3) {
 			case 1:
 				$world->setBlock(new Vector3($x,$y,$z), Block::get(4));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["player-message"]);
 			break;
 			case 2:
 				$world->dropItem(new Vector3($x,$y,$z), Item::get(1));
 				$this->playMusic($player);
+				$player->sendMessage($this->plugin->messages["player-message"]);
 			break;
 		}
 	}
